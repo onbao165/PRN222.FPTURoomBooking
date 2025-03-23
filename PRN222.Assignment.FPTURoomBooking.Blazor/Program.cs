ed6f58af118a5e5c6940fb8b13993676faea322d
khanght <khanghuynhtan.work@gmail.com>
2025-03-23 13:41:57 +0700

    remove

--- PRN222.Assignment.FPTURoomBooking.Blazor/Program.cs
+++ PRN222.Assignment.FPTURoomBooking.Blazor/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+
 builder.ConfigureDatabase();
 builder.ConfigureServices();
 builder.ConfigureCookieAuthentication();
