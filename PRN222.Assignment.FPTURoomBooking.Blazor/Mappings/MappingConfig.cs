e4a76b143d121f426b66994a8a2869f3e6f40db3
khanght <khanghuynhtan.work@gmail.com>
2025-03-31 12:33:18 +0700

    [REMOVE]: Remove room slot service

--- PRN222.Assignment.FPTURoomBooking.Blazor/Mappings/MappingConfig.cs
+++ PRN222.Assignment.FPTURoomBooking.Blazor/Mappings/MappingConfig.cs
@@ -1,7 +1,7 @@
 using Mapster;
 using PRN222.Assignment.FPTURoomBooking.Blazor.Models;
 using PRN222.Assignment.FPTURoomBooking.Services.Models.Booking;
-using PRN222.Assignment.FPTURoomBooking.Services.Models.RoomSlot;
+using PRN222.Assignment.FPTURoomBooking.Services.Models.Slot;
 
 namespace PRN222.Assignment.FPTURoomBooking.Blazor.Mappings;
 
@@ -13,7 +13,7 @@ public static class MappingConfig
         TypeAdapterConfig<BookingModel, BookingDetailsViewModel>.NewConfig()
             .Map(dest => dest.AccountName, src => src.Account.FullName);
         // Map RoomSlotModel to BookingRoomSlotViewModel
-        TypeAdapterConfig<RoomSlotModel, BookingSlotViewModel>.NewConfig()
+        TypeAdapterConfig<SlotModel, BookingSlotViewModel>.NewConfig()
             .Map(dest => dest.RoomName, src => src.Room.Name);
     }
 }
